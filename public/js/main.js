0b682b55e495aef645dd78b4c2e2fb0e0846bcdb
moollaza <moollaza@gmail.com>
2012-12-18 01:56:34 -0500

    reworking with row/span layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,35 @@
-$(document).ready(function() {
-
-  // Setup Gray Bars and their width/height/offset
+// Setup Gray Bars and their width/height/offset
+function bars(){
   $("#tag_container").width(($("#name").width()));
-  $("header .line").width((($("#name").width() - $("#tag").width())/2) - 1);
+  $("header .line").width((($("#name").width() - $("#tag").width())/2) - 2);
   var top_offset = $("#tag_container").outerHeight()/2 - $(".line").height()/2 + 1;
   $(".line").css("top", top_offset + "px");
-  
+}
+
+function size(){
+  $("#index section").css("width", ($("#slider").width()/3) - 20);
+}
+
+$(document).ready(function() {
+
+  bars();
+  size();
+
+  // Force media icons to appear in stacked row
+  var icon_width = $("#media_icons li a").outerWidth();
+  $("#media_icons").css("width", icon_width * 4 + (4 * 8));
+
   // Used to slide content sections horizontally
   $("#home_button").click(function() {
-    $("#content").css("left", ( 0 ));
+    $("#slider").css("left", ( 0 ));
   });  
 
   $("#contact_button").click(function() {
-    $("#content").css("left", ( -100 + "%" ));
-    console.log("LOLZ")
+    $("#slider").css("left", ( -100 + "%" ));
   });
 
   $("#about_button").click(function() {
-    $("#content").css("left", ( -200 + "%" ));
+    $("#slider").css("left", ( -200 + "%" ));
   });
 
   /* For Portfolio Page */
@@ -32,4 +44,5 @@ $(document).ready(function() {
 
 $(window).resize(function() {
     $content.css("left", ( 0 ) );
+    size();
 });

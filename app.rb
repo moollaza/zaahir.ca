f3b37fdbbeed70f8443f827e6ee0dbb22224188c
Zaahir Moolla <moollaza@gmail.com>
2012-12-30 18:20:53 -0500

    fixed <title> for each page, added new 404 page to app.rb

--- app.rb
+++ app.rb
@@ -5,20 +5,21 @@ require 'haml'
 require './config'
 
 get '/' do
-  @title = "Zaahir.ca -- Personal Website of Zaahir Moolla - Undergrad.Nerd.Musician  -- Home"
+  @title = "Zaahir.ca - Personal Website of Zaahir Moolla {Hacker, Nerd &amp; Musician}  - Home"
   haml :index
 end
 
 get '/portfolio' do
-	@title = "Zaahir.ca -- Personal Website of Zaahir Moolla - Undergrad.Nerd.Musician -- Portfolio"
+	@title = "Zaahir.ca - Personal Website of Zaahir Moolla {Hacker, Nerd &amp; Musician} - Portfolio"
 	haml :portfolio
 end
 
 get '/resume' do
-	@title = "Zaahir.ca -- Personal Website of Zaahir Moolla - Undergrad.Nerd.Musician -- Resume"
+	@title = "Zaahir.ca - Personal Website of Zaahir Moolla {Hacker, Nerd &amp; Musician} - Resume"
 	haml :resume
 end
 
 not_found do
-	redirect '/'
+	@title = "Zaahir.ca - Personal Website of Zaahir Moolla {Hacker, Nerd &amp; Musician} - Page Not Found!"
+	haml :not_found
 end

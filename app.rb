11372d412af074a9f05e82df9c078ca49045a8a8
Zaahir Moolla <moollaza@gmail.com>
2012-12-31 00:06:18 -0500

    added function to grab user agent and set size for social icons

--- app.rb
+++ app.rb
@@ -4,8 +4,25 @@ require 'sinatra/partial'
 require 'haml'
 require './config'
 
+before do
+	
+	# Create request object and get user agent
+	req = Rack::Request.new(env)
+	user_agent = req.user_agent()
+	
+	# Set social icon size based on user agent
+	@image_size = 64
+	case 
+		when user_agent.match(/ipad|playbook|nexus 7|nexus 10|tablet/i)
+				@image_size = 48
+		when user_agent.match(/iphone|android|blackberry/i)
+				@image_size = 32
+	end
+end
+
 get '/' do
   @title = "Zaahir.ca - Personal Website of Zaahir Moolla {Hacker, Nerd &amp; Musician}  - Home"
+
   haml :index
 end
 

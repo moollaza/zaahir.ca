0f4197295e0284c2f6cf1e5f359e708d9fc0e5f2
Zaahir Moolla <moollaza@fastmail.fm>
2019-11-13 02:52:32 -0500

    Setup tailwind css and fonts

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import './tailwind.css'
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+    .use(
+        compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, err => {
+        if (err) console.log('error', err);
+    });

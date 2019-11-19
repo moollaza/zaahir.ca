80e641b61e82f96457be27558335d24b55b675bc
Zaahir Moolla <moollaza@fastmail.fm>
2019-11-18 23:39:36 -0500

    Remove duplicate and unused styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,9 @@
   import Carousel from "../components/Carousel.svelte";
 </script>
 
-<style>
-  h1,
-  h2 {
-    /* text-shadow: 0px 1px 1px #000; */
-  }
-</style>
-
 <div class="home w-full h-full relative">
   <Carousel>
-    <span slot="title" class="text-6xl">ZAAHIR MOOLLA</span>
-    <span slot="subtitle" class="text-4xl">(a.k.a. moollaza)</span>
+    <span slot="title">ZAAHIR MOOLLA</span>
+    <span slot="subtitle">(a.k.a. moollaza)</span>
   </Carousel>
 </div>

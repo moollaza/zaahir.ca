f44069150742571bd75ff6fc54131507e7a4be89
Zaahir Moolla <moollaza@fastmail.fm>
2019-11-18 23:35:22 -0500

    Add Contact Page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,7 +1,40 @@
+<style>
+  img {
+    width: 75px;
+    filter: grayscale(100%);
+  }
+  img:hover {
+    animation: colourize 0.3s linear forwards;
+    animation-iteration-count: 1;
+  }
+
+  @keyframes colourize {
+    to {
+      filter: grayscale(0%);
+    }
+  }
+</style>
+
 <svelte:head>
   <title>Contact</title>
 </svelte:head>
 
-<h1>Get in touch</h1>
+<section class="m-auto">
+  <h1>Contact</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+  <span class="spacer" />
+
+  <p>Send me an email, or find me online</p>
+
+  <p>
+    <a href="mailto:info@zaahir.ca" class="hvr-float">
+      <img src="/icons/email.png" alt="at symbol" />
+    </a>
+    <a href="https://twitter.com/zmoolla" class="hvr-float">
+      <img src="/icons/twitter.png" alt="Twitter logo" />
+    </a>
+    <a href="https://github.com/moollaza" class="hvr-float">
+      <img src="/icons/github.png" alt="GitHub logo" />
+    </a>
+  </p>
+</section>

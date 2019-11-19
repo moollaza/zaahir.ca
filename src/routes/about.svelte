8664ab594743422fea4fc74968fbd4a3796a0187
Zaahir Moolla <moollaza@fastmail.fm>
2019-11-18 23:34:54 -0500

    Add About Page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,19 +1,60 @@
+<style>
+  img {
+    width: 40px;
+  }
+</style>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
-<section>
+<section class="m-auto">
   <h1>About Me</h1>
 
-  <span class="spacer" />
+  <hr class="spacer" />
 
   <p>
-    Hi. My name is Zaahir.
-    <br />
-    I'm a frontend developer at
+    Hi. My name is Zaahir. I'm from
+    <img src="/icons/canada.png" alt="Canada Flag" class="hvr-buzz-out" />
+    Canada.
+  </p>
+
+  <p>
+    I'm a
+    <img
+      src="/icons/javascript.png"
+      alt="Javascript logo"
+      class="hvr-buzz-out" />
+    frontend developer at
+    <img
+      src="/icons/duckduckgo.png"
+      alt="DuckDuckGo logo"
+      class="hvr-buzz-out" />
     <a href="https://duckduckgo.com">DuckDuckGo</a>
     .
-    <br />
-    I like technology, music, and movies.
+  </p>
+
+  <p>
+    I like
+    <img
+      src="/icons/music-record.png"
+      alt="vinyl record"
+      class="hvr-buzz-out" />
+    music,
+    <img
+      src="/icons/movie-projector.png"
+      alt="movie projector"
+      class="hvr-buzz-out" />
+    movies,
+    <img
+      src="/icons/controller.png"
+      alt="video game controller"
+      class="hvr-buzz-out" />
+    games, and all kinds of
+    <img
+      src="/icons/processor.png"
+      alt="computer procesor"
+      class="hvr-buzz-out" />
+    technology.
   </p>
 </section>

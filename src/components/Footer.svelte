258683c1756a1679dff32b9feb3f42fd4dae0143
Zaahir Moolla <moollaza@fastmail.fm>
2019-11-20 21:39:39 -0500

    Give credit where it's due

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,11 +4,13 @@
 
 <footer
   class:text-white={!segment}
-  class="absolute bottom-0 inset-x-0 flex items-center justify-center pt-6 pb-1">
-  <div>© 2019 Zaahir Moolla</div>
-  <div class="px-2">|</div>
-  <div>
+  class="fixed bottom-0 w-full flex items-center justify-center py-1">
+  <span>© 2019 Zaahir Moolla</span>
+  <span class="px-2">|</span>
+  <span>
     Icons by
     <a href="https://icons8.com" class="underline">Icons8</a>
-  </div>
+  </span>
+  <span class="px-2">|</span>
+  <span>Photos by Zaahir Moolla</span>
 </footer>

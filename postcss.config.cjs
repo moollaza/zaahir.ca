996d19ca2aa2a2b976eba946b9aef729c20a16d7
Zaahir Moolla <moollaza@fastmail.fm>
2021-04-11 17:53:44 -0400

    Migrate to Sveltekit

---
+++ postcss.config.cjs
@@ -0,0 +1,19 @@
+const cssnano = require('cssnano');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+module.exports = {
+	plugins: [
+		require('postcss-import'),
+		require('tailwindcss'),
+		require('postcss-nested'),
+		// require('postcss-custom-properties'),
+		require('autoprefixer'),
+
+		!dev &&
+			cssnano({
+				preset: 'default'
+			})
+	]
+};

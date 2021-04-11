36a5c9fc6521947e394cc6251f8d043494de5d3a
Zaahir Moolla <moollaza@fastmail.fm>
2021-04-11 18:59:32 -0400

    Revert "Migrate to Sveltekit"

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+  import Footer from "../components/Footer.svelte";
+
+  export let segment;
+</script>
+
+<Nav {segment} />
+
+<main
+  class="flex-auto flex"
+  class:items-stretch={segment === undefined}
+  class:relative={segment === undefined}>
+  <slot />
+</main>
+
+<Footer {segment} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Carousel from '$lib/Carousel.svelte';
+  import Carousel from "../components/Carousel.svelte";
 </script>
 
 <svelte:head>
-	<title>Zaahir Moolla - Personal Website | Frontend Dev</title>
+  <title>Zaahir Moolla - Personal Website | Frontend Dev</title>
 </svelte:head>
 
 <Carousel>
-	<span slot="title">ZAAHIR MOOLLA</span>
-	<span slot="subtitle">(a.k.a. moollaza)</span>
+  <span slot="title">ZAAHIR MOOLLA</span>
+  <span slot="subtitle">(a.k.a. moollaza)</span>
 </Carousel>
